914e2dcc3afb613f6228b827a08811f25483235f
Rom7699 <rom7699@gmail.com>
2025-04-16 13:10:19 +0300

    Initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+   # # Example 3: Save jobs to a JSON file
+    # with open("linkedin_jobs.json", "w") as f:
+    #     json.dump(jobs_dict, f, indent=2)
+    # print("\nSaved jobs to linkedin_jobs.json")
